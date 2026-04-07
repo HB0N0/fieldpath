1267b7c532723841d5741ccbfb672ec68a953e50
Hannes <hannes.bosch@uni-hohenheim.de>
2026-04-07 17:15:05 +0200

    Hotfix: Changed setup to auto find sub packages

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='fieldpath',
-    version='0.2.0',    
+    version='0.2.1',    
     description='Generate vector coverage paths for agricultural fields',
     url='https://github.com/HB0N0/FieldPath',
     author='Hannes Bosch',
     author_email='hbono@gmx.de',
     license='BSD 2-clause',
-    packages=['fieldpath'],
+    packages=find_packages(),
     install_requires=['shapely>=2.0',
                       'numpy',
                       'matplotlib',
