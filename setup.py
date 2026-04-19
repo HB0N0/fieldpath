16b55964b150c0fc8167ee3ac6958aff6f42a902
Hannes <hannes.bosch@uni-hohenheim.de>
2026-04-19 23:33:04 +0200

    Added min python version to setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='fieldpath',
-    version='0.2.2',    
+    version='0.2.2',
     description='Generate vector coverage paths for agricultural fields',
     url='https://github.com/HB0N0/FieldPath',
     author='Hannes Bosch',
     author_email='hbono@gmx.de',
     license='BSD 2-clause',
     packages=find_packages(),
+    python_requires='>=3.9',
     install_requires=['shapely>=2.0',
                       'numpy',
                       'matplotlib',
